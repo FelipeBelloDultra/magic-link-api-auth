78e3d6eaacb17b7f7b4e471b18fe4b4091a329e2
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-03-11 15:06:00 -0300

    chore: create project structure

---
+++ src/config/env.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const ENVIRONMENT_SCHEMA = z.object({
+  HTTP_SERVER_PORT: z.coerce.number().default(3000),
+});
+
+const parsedEnv = ENVIRONMENT_SCHEMA.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  throw new Error(
+    `Invalid env var: ${JSON.stringify(
+      parsedEnv.error.formErrors.fieldErrors,
+      undefined,
+      2
+    )}`,
+    {
+      cause: "Invalid env config",
+    }
+  );
+}
+
+export const env = parsedEnv.data;
