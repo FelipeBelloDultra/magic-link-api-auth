33913b9802a367a386600305fdf315981494014d
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-03-14 11:46:05 -0300

    fix: add try catch to handle if has error

--- src/infra/http/controllers/accounts/verify-token.ts
+++ src/infra/http/controllers/accounts/verify-token.ts
@@ -3,6 +3,9 @@ import { FastifyReply, FastifyRequest } from "fastify";
 
 import { makeVerifyToken } from "~/application/use-cases/factories/make-verify-token";
 
+import { InvalidTokenError } from "~/application/use-cases/errors/invalid-token-error";
+import { TokenExpiredError } from "~/application/use-cases/errors/token-expired-error";
+
 export async function verifyToken(
   request: FastifyRequest,
   reply: FastifyReply
@@ -12,10 +15,26 @@ export async function verifyToken(
   });
   const { token } = verifyTokenBodySchema.parse(request.params);
 
-  const verifyToken = makeVerifyToken();
-  const { authenticated_token } = await verifyToken.execute({ token });
+  try {
+    const verifyToken = makeVerifyToken();
+    const { authenticated_token } = await verifyToken.execute({ token });
 
-  return reply.status(200).send({
-    authenticated_token,
-  });
+    return reply.status(200).send({
+      authenticated_token,
+    });
+  } catch (error) {
+    if (error instanceof InvalidTokenError) {
+      return reply.status(401).send({
+        message: error.message,
+      });
+    }
+
+    if (error instanceof TokenExpiredError) {
+      return reply.status(403).send({
+        message: error.message,
+      });
+    }
+
+    throw error;
+  }
 }
